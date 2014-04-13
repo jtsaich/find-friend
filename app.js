df41ae261cf292e91d438d88e4f1edab6fc03475
Connie <connie@applecrateseo.com>
2014-04-13 12:56:39 -0700

    html for match page

--- app.js
+++ app.js
@@ -116,6 +116,9 @@ app.get('/account', ensureAuthenticated, function(req, res){
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
+app.get('/match', function(req, res){
+  res.render('match');
+});
 
 // GET /auth/facebook
 //   Use passport.authenticate() as route middleware to authenticate the
